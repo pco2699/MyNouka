591616206433b641f77f1b13471476a03292c7f2
pco2699 <aenkun@gmail.com>
2017-05-25 20:42:28 +0900

    FirstCommit

--- MyJamesBlake/ViewController.swift
+++ MyJamesBlake/ViewController.swift
@@ -7,11 +7,19 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
+    playt.layer.borderColor = UIColor.gray.cgColor
+    playt.layer.borderWidth = 1
+    
+    stopt.layer.borderColor = UIColor.gray.cgColor
+    stopt.layer.borderWidth = 1
+
+    
     // Do any additional setup after loading the view, typically from a nib.
   }
 
@@ -19,7 +27,60 @@ class ViewController: UIViewController {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
+  
+  // MP3のパスの設定
+  let noukaPath = Bundle.main.bundleURL.appendingPathComponent("nouka.aif")
+  let itu_noukaPath = Bundle.main.bundleURL.appendingPathComponent("itu_nouka.aif")
 
+  let rakunoukaPath = Bundle.main.bundleURL.appendingPathComponent("rakunouka.aif")
+  let itu_rakunoukaPath = Bundle.main.bundleURL.appendingPathComponent("itu_rakunouka.aif")
 
+  
+  
+  let backmusicPath = Bundle.main.bundleURL.appendingPathComponent("NOUKA.wav")
+  
+  // Playerのインスタンス生成
+  var noukaPlayer = AVAudioPlayer()
+  var itu_noukaPlayer = AVAudioPlayer()
+  
+  var rakunoukaPlayer = AVAudioPlayer()
+  var itu_rakunoukaPlayer = AVAudioPlayer()
+
+  var backmusicPlayer = AVAudioPlayer()
+  
+  
+  @IBOutlet weak var playt: UIButton!
+  @IBOutlet weak var stopt: UIButton!
+  @IBAction func button1(_ sender: Any) {
+    soundPlayer(&noukaPlayer, path: noukaPath, count: 0)
+  }
+
+  @IBAction func button2(_ sender: Any) {
+    soundPlayer(&itu_noukaPlayer, path: itu_noukaPath, count: 0)
+  }
+  
+  @IBAction func button3(_ sender: Any) {
+    soundPlayer(&rakunoukaPlayer, path: rakunoukaPath, count: 0)
+  }
+  @IBAction func button4(_ sender: Any){
+    soundPlayer(&itu_rakunoukaPlayer, path: itu_rakunoukaPath, count: 0)
+  }
+  
+  @IBAction func play(_ sender: Any) {
+    soundPlayer(&backmusicPlayer, path: backmusicPath, count: -1)
+  }
+  @IBAction func stop(_ sender: Any) {
+    backmusicPlayer.stop()
+  }
+  
+  fileprivate func soundPlayer(_ player:inout AVAudioPlayer, path: URL, count: Int) {
+    do {
+      player = try AVAudioPlayer(contentsOf: path, fileTypeHint: nil)
+      player.numberOfLoops = count
+      player.play()
+    } catch {
+      print("エラーが発生しました！")
+    }
+  }
 }
 
